abd24e8b141bf17c0f99835367367b5147604c10
xiaxuchen <xiaxuchen@automq.com>
2024-09-02 12:32:43 +0800

    add log

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { PostGithubEvent } from './github2feishu'
+import * as core from '@actions/core'
 
+core.info('PostGithubEvent')
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 PostGithubEvent()
